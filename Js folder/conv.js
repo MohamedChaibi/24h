1bf76e258c1154ee312543144339cdcc09064752
MohamedChaibi <hamma_chaibi@yahoo.fr>
2025-05-24 12:05:19 +0100

    Project1

---
+++ Js folder/conv.js
@@ -0,0 +1,6 @@
+
+    var flousi = {
+    USD : { USD: 1 , EUR: 0.92 , TND: 3.1 },
+    EUR : { USD: 1.09 , EUR: 1 , TND: 3.35 },
+    TND : { USD: 0.32 , EUR: 0.3 , TND: 1 },
+};
---
+++ Js folder/km.js
@@ -0,0 +1,6 @@
+var jaryaaa = {
+    Millimeter: { Millimeter: 1 , Centimeter: 0.1 , Kilometer: 0.000001 , Mile: 0.0000006214 },
+    Centimeter: { Millimeter: 10 , Centimeter: 1 , Kilometer: 0.00001 , Mile: 0.000006214 },
+    Kilometer: { Millimeter: 1000000 , Centimeter: 100000 , Kilometer: 1, Mile: 0.6214 },
+    Mile: { Millimeter: 1609344 , Centimeter: 160934.4 , Kilometer: 1.609, Mile: 1 },
+};
