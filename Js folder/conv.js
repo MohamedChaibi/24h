4950ab711f354814ea07c885c119d8ac9dfcf307
MohamedChaibi <hamma_chaibi@yahoo.fr>
2025-05-30 15:05:59 +0100

    done

--- Js folder/conv.js
+++ Js folder/conv.js
@@ -1,6 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var inputFrom = document.querySelectorAll('.input')[0];
+    var inputTo = document.querySelectorAll('.input')[1];
+    var from = document.getElementById('money');
+    var to = document.getElementById('money_2');
+
 
     var flousi = {
-    USD : { USD: 1 , EUR: 0.92 , TND: 3.1 },
-    EUR : { USD: 1.09 , EUR: 1 , TND: 3.35 },
-    TND : { USD: 0.32 , EUR: 0.3 , TND: 1 },
+    USD: { USD: 1, EUR: 0.92, TND: 3.1 },
+    EUR: { USD: 1.09, EUR: 1, TND: 3.35 },
+    TND: { USD: 0.32, EUR: 0.3, TND: 1 },
 };
+
+
+document.querySelector('.btn').onclick = function () {
+    var amount = parseInt(inputFrom.value); 
+    inputTo.value = (amount * flousi[from.value][to.value]).toFixed(2);
+};
+});
--- Js folder/km.js
+++ Js folder/km.js
@@ -1,6 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+var inputFrom = document.querySelectorAll('.input')[0];
+var inputTo = document.querySelectorAll('.input')[1];
+var from = document.getElementById('money');
+var to = document.getElementById('money_2');
+
 var jaryaaa = {
-    Millimeter: { Millimeter: 1 , Centimeter: 0.1 , Kilometer: 0.000001 , Mile: 0.0000006214 },
-    Centimeter: { Millimeter: 10 , Centimeter: 1 , Kilometer: 0.00001 , Mile: 0.000006214 },
-    Kilometer: { Millimeter: 1000000 , Centimeter: 100000 , Kilometer: 1, Mile: 0.6214 },
-    Mile: { Millimeter: 1609344 , Centimeter: 160934.4 , Kilometer: 1.609, Mile: 1 },
+    Millimeter: { Millimeter: 1, Centimeter: 0.1, Kilometer: 0.000001, Mile: 0.0000006214 },
+    Centimeter: { Millimeter: 10, Centimeter: 1, Kilometer: 0.00001, Mile: 0.000006214 },
+    Kilometer: { Millimeter: 1000000, Centimeter: 100000, Kilometer: 1, Mile: 0.6214 },
+    Mile: { Millimeter: 1609344, Centimeter: 160934.4, Kilometer: 1.609, Mile: 1 },
 };
+
+document.querySelector('.btn').onclick = function () {
+    var amount = parseFloat(inputFrom.value);
+    inputTo.value = (amount * jaryaaa[from.value][to.value]).toFixed(5);
+};
+});
